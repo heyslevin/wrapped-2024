78ab5b87c996609c5c00a7b840766c3f7ac106da
Manuel <memellaguno@gmail.com>
2024-12-26 23:10:09 -0600

    hero with logo

--- nextjs-app/app/components/blocks/TestBlock.tsx
+++ nextjs-app/app/components/blocks/TestBlock.tsx
@@ -1,6 +1,10 @@
+import { urlForImage } from "@/sanity/lib/utils";
 import styles from "../../styles/heroStyles.module.css";
+import { Image } from "next-sanity/image";
 
 export default function TestBlock({ block }: any) {
+  const image = block.logo;
+  console.log({ test: block });
   return (
     <main>
       {/* Hero block */}
@@ -26,11 +30,20 @@ export default function TestBlock({ block }: any) {
                 WRAPPED 2024
               </h1>
             </div>
-            <div className="rounded-lg border border-white bg-none p-5 text-5xl text-white">
-              Welcome to my website bitch. Welcome to my website bitch Welcome
-              to my website bitch Welcome to my website bitch Welcome to my
-              website bitch Welcome to my website bitch Welcome to my website
-              bitch Welcome to my website bitch Welcome to my website bitch
+            <div className="flex flex-col justify-between rounded-lg border border-white bg-none p-5 text-white">
+              <div className="relative w-full">
+                <Image
+                  className="w-full"
+                  src={urlForImage(image)?.url() as string}
+                  alt={image?.alt || ""}
+                  width={100}
+                  height={50}
+                />
+              </div>
+              <div className="text-balance text-3xl">
+                12 years making things easy to understand, and impossible to
+                ignore.
+              </div>
             </div>
           </div>
         </div>
