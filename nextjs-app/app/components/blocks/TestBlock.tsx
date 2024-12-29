8a676650825b052578ab8ab1181bd8510b2dbdd3
Manuel <memellaguno@gmail.com>
2024-12-28 23:59:51 -0600

    paragraph block mobile and desktop ready

--- nextjs-app/app/components/blocks/TestBlock.tsx
+++ nextjs-app/app/components/blocks/TestBlock.tsx
@@ -27,30 +27,30 @@ export default function TestBlock({ block }: any) {
           <div className={`${styles.row} ${styles["row-10"]} `}></div>
         </div>
         {/* Content */}
-        <div className="md:p-0 md:mx-10 relative mx-1 flex h-full w-full p-4">
+        <div className="relative mx-1 flex h-full w-full p-4 md:mx-10 md:p-0">
           <div className="grid w-full grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-6">
             {/* Left block */}
-            <section className="motion-opacity-in-0 motion-blur-in-md motion-translate-y-in-25 flex h-full min-h-80 flex-col justify-between rounded-lg bg-black text-white">
+            <section className="flex h-full min-h-80 flex-col justify-between rounded-lg bg-black text-white motion-translate-y-in-25 motion-blur-in-md motion-opacity-in-0">
               <div className="p-5">
-                <h1 className="motion-opacity-in-0 motion-blur-in-md motion-translate-y-in-25 motion-delay-500 md:text-[6vw] md:leading-[5vw] flex h-full px-0 text-[3.5rem] font-black leading-[3rem] tracking-tight">
+                <h1 className="flex h-full px-0 text-[3.5rem] font-black leading-[3rem] tracking-tight motion-translate-y-in-25 motion-blur-in-md motion-opacity-in-0 motion-delay-500 md:text-[6vw] md:leading-[5vw]">
                   WRAPPED 2024
                 </h1>
               </div>
-              <nav className="md:text-2xl text-xl">
+              <nav className="text-xl md:text-2xl">
                 <a href="" className="group">
-                  <div className="motion-opacity-in-0 motion-blur-in-md motion-translate-y-in-25 motion-delay-[700ms] flex items-center justify-between border-y px-5 py-2">
+                  <div className="flex items-center justify-between border-y px-5 py-2 motion-translate-y-in-25 motion-blur-in-md motion-opacity-in-0 motion-delay-[700ms]">
                     <p className="">The Stats</p>
                     <ArrowRight className="group-hover:motion-preset-wobble-md" />
                   </div>
                 </a>
                 <a href="" className="group">
-                  <div className="motion-opacity-in-0 motion-blur-in-md motion-translate-y-in-25 motion-delay-[900ms] flex items-center justify-between border-b px-5 py-2">
+                  <div className="flex items-center justify-between border-b px-5 py-2 motion-translate-y-in-25 motion-blur-in-md motion-opacity-in-0 motion-delay-[900ms]">
                     <p className="">Iniciatives</p>
                     <ArrowRight className="group-hover:motion-preset-wobble-md" />
                   </div>
                 </a>
                 <a href="" className="group">
-                  <div className="motion-opacity-in-0 motion-blur-in-md motion-translate-y-in-25 motion-delay-[1100ms] flex items-center justify-between px-5 py-2">
+                  <div className="flex items-center justify-between px-5 py-2 motion-translate-y-in-25 motion-blur-in-md motion-opacity-in-0 motion-delay-[1100ms]">
                     <p className="">Tools</p>
                     <ArrowRight className="group-hover:motion-preset-wobble-md" />
                   </div>
@@ -59,8 +59,8 @@ export default function TestBlock({ block }: any) {
             </section>
 
             {/* Right Block */}
-            <section className="motion-opacity-in-0 motion-blur-in-md motion-translate-y-in-25 flex min-h-80 flex-col justify-between rounded-lg border border-white bg-none p-5 text-white">
-              <div className="motion-opacity-in-0 motion-blur-in-md motion-translate-y-in-25 motion-delay-500 relative w-full">
+            <section className="flex min-h-80 flex-col justify-between rounded-lg border border-white bg-none p-5 text-white motion-translate-y-in-25 motion-blur-in-md motion-opacity-in-0">
+              <div className="relative w-full motion-translate-y-in-25 motion-blur-in-md motion-opacity-in-0 motion-delay-500">
                 <Image
                   className="w-full"
                   src={urlForImage(image)?.url() as string}
@@ -69,7 +69,7 @@ export default function TestBlock({ block }: any) {
                   height={50}
                 />
               </div>
-              <div className="motion-preset-slide-up-md motion-blur-in-md motion-delay-1500 md:text-3xl text-balance text-2xl">
+              <div className="motion-preset-slide-up-md text-balance text-2xl motion-blur-in-md motion-delay-1500 md:text-3xl">
                 12 years making things easy to understand, and impossible to
                 ignore.
               </div>
@@ -79,9 +79,9 @@ export default function TestBlock({ block }: any) {
       </BlockContainer>
 
       {/* Paragraph Section */}
-      <BlockContainer>
-        <div className="md:w-1/2 md:py-8 w-full">
-          <h2 className="text-2xl">
+      <BlockContainer className="px-5 py-24 md:px-0">
+        <div className="w-full md:w-1/2 md:py-8">
+          <h2 className="text-xl md:text-2xl">
             We’ve been on the brink of change for several years, and this past
             year was an acceleration towards the world ahead. What do we want it
             to look like? We want to be active participants by advocating for a
