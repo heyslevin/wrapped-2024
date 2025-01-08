27d392f81cc8fede56ac44532879b1c801b38284
Manuel <memellaguno@gmail.com>
2025-01-07 21:54:42 -0600

    speed insight

--- nextjs-app/app/(home)/layout.tsx
+++ nextjs-app/app/(home)/layout.tsx
@@ -1,6 +1,7 @@
 import "../globals.css";
 
 import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { draftMode } from "next/headers";
